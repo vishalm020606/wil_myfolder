43080a9f5026c667475eba4c760c1dda88aadf63
vishalm020606 <vishalmunu2006@gmail.com>
2026-05-26 14:51:15 +0530

    Updated MySQL Port

--- register.php
+++ register.php
@@ -1,4 +1,10 @@
 <?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'PHPMailer/src/Exception.php';
+require 'PHPMailer/src/PHPMailer.php';
+require 'PHPMailer/src/SMTP.php';
 
 error_reporting(E_ALL);
 ini_set('display_errors',1);
@@ -11,7 +17,7 @@ DATABASE CONNECTION
 */
 
 $conn = mysqli_connect(
-    "localhost",
+   "localhost:3307",
     "root",
     "",
     "wil_company"
@@ -127,6 +133,90 @@ if(isset($_POST['register'])){
 
 
     if(mysqli_query($conn,$sql)){
+        $mail = new PHPMailer(true);
+
+try{
+
+    $mail->isSMTP();
+
+    $mail->Host = 'smtp.gmail.com';
+
+    $mail->SMTPAuth = true;
+
+    $mail->Username = 'vishal@gmail.com';
+
+    $mail->Password = 'abcd efgh ijkl mnop';
+
+    $mail->SMTPSecure = 'tls';
+
+    $mail->Port = 587;
+
+
+    $mail->setFrom(
+        'yourgmail@gmail.com',
+        'Wheels India Limited'
+    );
+
+    $mail->addAddress($email);
+
+
+    $mail->isHTML(true);
+
+    $mail->Subject =
+    'Registration Successful';
+
+
+    $mail->Body = "
+
+    <div style='font-family:Arial;
+                padding:20px;'>
+
+    <h2 style='color:#007bff;'>
+
+    Wheels India Limited
+
+    </h2>
+
+    <p>
+
+    Hello <b>$fullname</b>,
+
+    </p>
+
+    <p>
+
+    Your registration has been
+    completed successfully.
+
+    </p>
+
+    <p>
+
+    Username:
+    <b>$username</b>
+
+    </p>
+
+    <p>
+
+    Welcome to Travel Management
+    System.
+
+    </p>
+
+    </div>
+
+    ";
+
+
+    $mail->send();
+
+}
+catch(Exception $e){
+
+    echo $mail->ErrorInfo;
+
+}
 
         echo "
 
